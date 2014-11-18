87806770968222eaee05e31b469f9edc9a0ef684
ed <ejbyne@gmail.com>
2014-11-18 13:59:15 +0000

    added password confirmation step

--- app/server.rb
+++ app/server.rb
@@ -37,19 +37,11 @@ class BookmarkManager < Sinatra::Base
 
   post '/users' do
     user = User.create( :email => params[:email],
-                        :password => params[:password])
+                        :password => params[:password],
+                        :password_confirmation => params[:password_confirmation])
     session[:user_id] = user.id
     redirect to('/')
   end
 
-  helpers do
-
-    def current_user
-      @current_user ||= User.get(session[:user_id]) if session[:user_id]
-    end
-
-  end
-
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
