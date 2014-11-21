1fcba7817d8739cd51bb3d3e43e97afb401b29b3
ed <ejbyne@gmail.com>
2014-11-21 13:47:02 +0000

    amended heroku config

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User
   validates_confirmation_of :password
   # validates_uniqueness_of :email
 
+  MAILGUN_API = ENV['MAILGUN_API']
+  BOOKMARK_URL = ENV['BOOKMARK_URL']
+
   property :id,                       Serial
   property :email,                    String,  :unique => true,  :message => "This email is already taken"
   property :password_digest,          Text
@@ -34,13 +37,12 @@ class User
 
 
   def send_message
-    api = ENV['MAILGUN_API']
-    RestClient.post "https://#{api}"\
+    RestClient.post "https://#{MAILGUN_API}"\
     "@api.mailgun.net/v2/sandbox4478906f6e744d7e900a98a5c2509c4c.mailgun.org/messages",
     :from => "Mailgun Sandbox <postmaster@sandbox4478906f6e744d7e900a98a5c2509c4c.mailgun.org>",
     :to => "Edward Byne <ejbyne@gmail.com>",
     :subject => "Bookmark Manager - password reset email",
-    :text => "Hello #{self.email}. Please follow this link within one hour to change your email: #{@base_url} http://localhost:9292/users/change_password/#{self.password_token}."
+    :text => "Hello #{self.email}. Please follow this link within one hour to change your email: #{BOOKMARK_URL}/users/change_password/#{self.password_token}."
   end
 
 end
